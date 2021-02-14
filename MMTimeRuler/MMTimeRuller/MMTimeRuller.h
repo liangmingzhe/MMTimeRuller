99bbdde971cd7555d269c99f2651574d44741468
liangmingzhe <benjaminlmz@qq.com>
2021-02-14 14:24:59 +0800

    first commit

---
+++ MMTimeRuler/MMTimeRuller/MMTimeRuller.h
@@ -0,0 +1,16 @@
+//
+//  MMTimeRuller.h
+//  MMTimeRuler
+//
+//  Created by benjaminlmz@qq.com on 2021/2/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMTimeRuller : UIScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
