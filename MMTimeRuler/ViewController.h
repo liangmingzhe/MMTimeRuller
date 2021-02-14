5074fe98c7b54617b999f165c4066eb1e8b7c50d
liangmingzhe <benjaminlmz@qq.com>
2021-02-14 12:49:13 +0800

    Initial Commit

---
+++ MMTimeRuler/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MMTimeRuler
+//
+//  Created by benjaminlmz@qq.com on 2021/2/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MMTimeRuler/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MMTimeRuler
+//
+//  Created by benjaminlmz@qq.com on 2021/2/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
